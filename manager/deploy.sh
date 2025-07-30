7381f72f861fb03b3b2da04b245bb3426d85866f
Amir Hossein Rajabi <rajabi@taha.ai>
2025-07-30 16:14:13 +0330

    connect client & manager and making smal changes

--- manager/deploy.sh
+++ manager/deploy.sh
@@ -1,4 +1,4 @@
-docker-compose -f ./deployment/docker-compose.yml down
-docker-compose -f ./deployment/docker-compose-infrastructure.yml down
-docker-compose -f ./deployment/docker-compose-infrastructure.yml up -d
-docker-compose -f ./deployment/docker-compose.yml up
+docker compose -f ./deployment/docker-compose.yml down
+docker compose -f ./deployment/docker-compose-infrastructure.yml down
+docker compose -f ./deployment/docker-compose-infrastructure.yml up -d
+docker compose -f ./deployment/docker-compose.yml up
